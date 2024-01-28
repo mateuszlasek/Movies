8a3550b3a6d227a13f2512ee93e0463ac6b7e9d4
mateuszlasek <mateusz.lasek17@wp.pl>
2024-01-28 23:07:42 +0100

    small changes

--- assets/app.js
+++ assets/app.js
@@ -8,8 +8,5 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-//Compile new JS file
-import './javascript/method1.js'
-
 // start the Stimulus application
 import './bootstrap';
